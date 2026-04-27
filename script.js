30db73312cf6da8e455df552f388a5df0b01879a
M.Bilal Waris <bilalwaris543@gmail.com>
2026-04-27 23:12:02 +0500

    Integrated Images through JS

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let rough = ""
 
-let showcars = 
+let showcars = [
+    {
+        "id": "1",
+        "title": "Ferrari",
+        "img": "https://idsb.tmgrup.com.tr/ly/uploads/images/2021/05/30/118050.jpg"
+    },
+    {
+        "id": "2",  
+        "title": "Civic",
+        "img": "https://imgd.aeplcdn.com/370x208/n/cw/ec/27074/civic-exterior-right-side-view.jpeg?q=80"
+    },
+    {
+        "id": "3",
+        "title": "Lamborghini",
+        "img": "http://127.0.0.1:5500/assets/carousel-image.jpg"
+    }
+]
 
 function showallcars() {
     showcars.forEach(element => {
